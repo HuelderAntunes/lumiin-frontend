1b1452985ad9f3eea649e957065d3336123a1b14
FahRamatis <fah.ramatis@gmail.com>
2019-12-03 17:06:07 -0300

    document form

--- src/pages/Preferences/forms/DocumentForm.jsx
+++ src/pages/Preferences/forms/DocumentForm.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
-import { Result } from 'antd'
+import React, { useState } from 'react'
+import { Form, Input, Button, Row, Col } from 'antd'
 
-const DocumentForm = (props) => {
-  return (<Result
-    status='404'
-    title='Em construção'
-    subTitle='Essa pagina estara disponível para acesso em breve.'
-  />)
+const DocumentForm = props => {
+  const { getFieldDecorator } = props.form
+  const [forms, setForms] = useState([
+    { label: 'Documento', value: '' },
+    { label: 'Foto', value: '' }
+  ])
+  function handleSubmit(e) {
+    e.preventDefault()
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values)
+      }
+    })
+  }
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Row>
+          <Col span={8}>
+            {forms.map(form => (
+              <Form.Item label={form.label}>
+                {getFieldDecorator('document', {
+                  initialValue: form.value,
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Digite um documento válido.',
+                      whitespace: false
+                    }
+                  ]
+                })(<Input />)}
+              </Form.Item>
+            ))}
+            <Button
+              type="primary"
+              icon="plus"
+              onClick={() =>
+                setForms([
+                  ...forms,
+                  { label: 'Documento', value: '' },
+                  { label: 'Foto', value: '' }
+                ])
+              }
+            >
+              Adicionar novo documento
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  )
 }
 
-export default DocumentForm
+export default Form.create({ name: 'document_form' })(DocumentForm)
