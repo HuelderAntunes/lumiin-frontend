94c41ce50318f74fc0eabfda1cf7f5f9974394cb
FahRamatis <fah.ramatis@gmail.com>
2019-12-04 19:57:26 -0300

    document form finished(need some polish details

--- src/pages/Preferences/forms/DocumentForm.jsx
+++ src/pages/Preferences/forms/DocumentForm.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { Form, Input, Button, Row, Col } from 'antd'
+import { Form, message, Input, Icon, Button, Row, Col, Upload } from 'antd'
+const { Dragger } = Upload
 
 const DocumentForm = props => {
   const { getFieldDecorator } = props.form
   const [forms, setForms] = useState([
-    { label: 'Documento', value: '' },
-    { label: 'Foto', value: '' }
+    { label: 'Documento', value: '', type: 'form' },
+    { label: 'Foto', value: '', type: 'image' }
   ])
   function handleSubmit(e) {
     e.preventDefault()
@@ -15,38 +16,77 @@ const DocumentForm = props => {
       }
     })
   }
+
+  function handleForm(form) {
+    if (form.type === 'form') {
+      return (
+        <Form.Item label={form.label}>
+          {getFieldDecorator('document', {
+            initialValue: form.value,
+            rules: [
+              {
+                required: true,
+                message: 'Digite um documento válido.',
+                whitespace: false
+              }
+            ]
+          })(<Input />)}
+        </Form.Item>
+      )
+    } else if (form.type === 'image') {
+      return (
+        <Dragger
+          name="file"
+          multiple={true}
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          onChange={info => {
+            const { status } = info.file
+            if (status !== 'uploading') {
+              console.log(info.file, info.fileList)
+            }
+            if (status === 'done') {
+              message.success(`${info.file.name} file uploaded successfully`)
+            } else if (status === 'error') {
+              message.error(`${info.file.name} file uploaded failed.`)
+            }
+          }}
+        >
+          <p className="ant-upload-drag-icon">
+            <Icon type="inbox" />
+          </p>
+          <p className="ant-upload-text">
+            Clique ou arraste o arquivo para esta área
+          </p>
+          <p className="ant-upload-hint">
+            Suporte apenas para unicos uploads ou uploads em massa. Estritamente
+            proibido fazer upload de arquivos de empresas ou outros tipos.
+          </p>
+        </Dragger>
+      )
+    }
+  }
   return (
     <div>
       <Form onSubmit={handleSubmit}>
         <Row>
           <Col span={8}>
-            {forms.map(form => (
-              <Form.Item label={form.label}>
-                {getFieldDecorator('document', {
-                  initialValue: form.value,
-                  rules: [
-                    {
-                      required: true,
-                      message: 'Digite um documento válido.',
-                      whitespace: false
-                    }
-                  ]
-                })(<Input />)}
-              </Form.Item>
-            ))}
-            <Button
-              type="primary"
-              icon="plus"
-              onClick={() =>
-                setForms([
-                  ...forms,
-                  { label: 'Documento', value: '' },
-                  { label: 'Foto', value: '' }
-                ])
-              }
-            >
-              Adicionar novo documento
-            </Button>
+            {forms.map(form => handleForm(form))}
+            <Row type="flex" justify="end">
+              <Button
+                type="ghost"
+                icon="plus"
+                style={{ alignSelf: 'flex-end', marginTop: 10 }}
+                onClick={() =>
+                  setForms([
+                    ...forms,
+                    { label: 'Documento', value: '', type: 'form' },
+                    { label: 'Foto', value: '', type: 'image' }
+                  ])
+                }
+              >
+                Adicionar novo documento
+              </Button>
+            </Row>
           </Col>
         </Row>
       </Form>
